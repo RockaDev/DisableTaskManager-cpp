2e5d1d23deb9e26816a47c2f157792cba6d285d5
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-11-23 22:47:26 +0100

    Update header.h

--- enable/header.h
+++ enable/header.h
@@ -7,18 +7,39 @@
 
 class Registry {
 private:
+
+	LPCWSTR DisableTaskMgr = TEXT("DisableTaskMgr");
 	HKEY hKey = HKEY_CURRENT_USER;
-	LPCWSTR TMGR_PATH = (L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\");
+	LPCWSTR TMGR_PATH = (L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\");
+	DWORD val_disable = 1;
+	DWORD val_enable = 0;
 
 public:
-	void ModifyRegistry();
+	void EnableT_MGR();
 };
 
-void Registry::ModifyRegistry()
+void Registry::EnableT_MGR()
 {
 	RegOpenKeyEx(hKey, TMGR_PATH, 0, KEY_ALL_ACCESS, &hKey);
 
-	RegDeleteKeyEx(hKey, L"System", KEY_WOW64_64KEY, NULL);
+	if (RegQueryValueEx(hKey, DisableTaskMgr, NULL, NULL, NULL, NULL) == ERROR_FILE_NOT_FOUND)
+	{
+
+		if (RegSetValueEx(hKey, DisableTaskMgr, 0, REG_DWORD, (const BYTE*)&val_enable, sizeof(val_enable)) != ERROR_SUCCESS)
+		{
+			return;
+		}
+		else
+		{
+			RegCloseKey(hKey);
+		}
+	}
+	else
+	{
+		RegSetValueEx(hKey, DisableTaskMgr, 0, REG_DWORD, (const BYTE*)&val_enable, sizeof(val_enable));
+		RegCloseKey(hKey);
+		return;
+	}
 
 	return;
 }
